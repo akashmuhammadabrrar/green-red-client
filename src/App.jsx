80edcade79f88de73ce6f82fcb31181f754c2350
AkashMAbrrar <madborakash48@gmail.com>
2025-01-04 17:56:03 +0600

    this is first commit for set up the project with react router dom

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl">Vite + React</h1>
+      <button className="btn btn-accent">Button</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <h2>404 not found</h2>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
